ad4723d3b7b16a596f34bdba41b4e463355c8306
Hardt, Simon <simon.hardt@loewen.de>
2018-10-31 11:17:21 +0100

    upgrade to androidx

--- app/src/main/java/de/hardysim/groupie_demo/MainActivity.kt
+++ app/src/main/java/de/hardysim/groupie_demo/MainActivity.kt
@@ -1,12 +1,22 @@
 package de.hardysim.groupie_demo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 
 class MainActivity : AppCompatActivity() {
+
+    private val groupAdapter by lazy {
+        object: GroupAdapter<ViewHolder>() {
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        groupAdapter.itemCount
     }
 }
